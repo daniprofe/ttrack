d74cbac19fe8a216694e1b8e23f906175d9a692c
Daniel Aparicio <dani@on4u.es>
2018-04-19 18:31:50 +0200

    First attempt to show startedAt and ellapsed

--- src/components/tracker/tracker.ts
+++ src/components/tracker/tracker.ts
@@ -1,22 +1,45 @@
 import { Component } from '@angular/core';
 
-/**
- * Generated class for the TrackerComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
+import { Observable } from 'rxjs/Rx';
+import moment from 'moment';
+
 @Component({
-  selector: 'ttracker-tracker',
-  templateUrl: 'tracker.html'
+    selector: 'ttracker-tracker',
+    templateUrl: 'tracker.html'
 })
+
 export class TrackerComponent {
 
-  text: string;
+    private locale: string = 'es';
+    private timer: Observable|null = null;
+    private startedAtMoment = null;
+    private startedAtString: string = '';
+    private ellapsedDuration = null;
+    private ellapsedString: string = '';
 
-  constructor() {
-    console.log('Hello TrackerComponent Component');
-    this.text = 'Hello World';
-  }
+    constructor() {
+        moment.locale(this.locale);
+        this.timer = Observable.timer(0, 1000);
+        this.timer.subscribe(t => { this.updateTimer(t); });
+    }
+
+    updateTimer(t) {
+
+        if (this.startedAtMoment !== null) {
+            this.startedAtString = this.startedAtMoment.calendar();
+            this.ellapsedDuration = moment.duration(moment().diff(this.startedAtMoment));
+
+            /* Tricky way to format a moment duration as HH:mm:ss
+             * https://github.com/moment/moment/issues/1048
+             */
+            this.ellapsedString = Math.floor(this.ellapsedDuration.asHours()) + moment.utc(this.ellapsedDuration.asMilliseconds()).format(":mm:ss");
+        }
+        // console.error('*');
+    }
+
+    startButtonClicked() {
+        this.startedAtMoment = moment();
+        this.startedAtString = this.startedAtMoment.calendar();
+    }
 
 }
