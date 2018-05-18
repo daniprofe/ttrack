d1ab46ae08663f10843bc3c234de607fe1dc5b3e
Daniel Aparicio <dani@on4u.es>
2018-05-18 18:08:40 +0200

    New user register & login requests (first version)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { TrackerInterval } from '../models/tracker-interval';
-import { Events, Nav } from 'ionic-angular';
+import { Content, Events, Nav } from 'ionic-angular';
 
 import { TaskListPage } from '../pages/task-list/task-list';
 import { LoginPage } from '../pages/login/login';
@@ -13,16 +13,23 @@ import { UserProvider } from '../providers/user';
 
 export class TTracker {
 
+    @ViewChild(Content) content: Content;
     @ViewChild(Nav) nav: Nav;
 
     rootPage: any = TaskListPage;
 
     constructor(
+        private events: Events,
         private user: UserProvider) {
+
+            this.events.subscribe('auth:login', () => {
+                this.content.resize();
+                this.nav.setRoot(TaskListPage);
+                console.error('logged in!!!!');
+            });
     }
 
     goToLoginPage() {
         this.nav.push(LoginPage);
-        // this.rootPage = LoginPage;
     }
 }
